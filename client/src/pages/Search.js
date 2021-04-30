b69eb09b912bafd7e45c76b80676260e44bf2a91
Daniel Lucas <danomiterock@yahoo.com>
2021-04-30 07:57:03 -0700

    create Search.js and handle functions

---
+++ client/src/pages/Search.js
@@ -0,0 +1,20 @@
+const baseUrl = "https://www.googleapis.com/books/v1/volumes?q=";
+const [search, setSearch] = useState("Tom Clancy");
+const [results, setResults] = useState({ items: [] });
+const handleInputChange = (e) => {
+    const { value } = e.target;
+    setSearch(value);
+  }
+const handleFormSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .get(baseUrl + search)
+      .then(res => setResults(res.data))
+      .catch(err => console.log(err))
+  }
+  useEffect(() => {
+    axios
+      .get(baseUrl + search)
+      .then(res => setResults(res.data))
+      .catch(err => console.log(err))
+  }, []);
