4777652e3769c95655fcdbc95886a312b6ad12c3
Daniel Lucas <danomiterock@yahoo.com>
2021-04-30 20:12:57 -0700

    npm run build

---
+++ client/build/precache-manifest.c9d6c0c87471af263936b4d16d8f7ed9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e5685968ae13a8dc1aa144916dd30c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98e35ab4f51c5374aa78",
+    "url": "/static/css/main.66f57dc2.chunk.css"
+  },
+  {
+    "revision": "6f19f7a0305c54873e3d",
+    "url": "/static/js/2.ab6ae14d.chunk.js"
+  },
+  {
+    "revision": "540787c921a5c2f895d35b81157288c0",
+    "url": "/static/js/2.ab6ae14d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98e35ab4f51c5374aa78",
+    "url": "/static/js/main.450066ca.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
