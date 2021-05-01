52a7775d628233707bea424a54c61b112013ee5d
Daniel Lucas <danomiterock@yahoo.com>
2021-04-30 19:57:40 -0700

    create book.js for html

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
+  
 const path = require("path");
 const router = require("express").Router();
-const apiRoutes = require("./api");
 
-// API Routes
-router.use("/api", apiRoutes);
+const htmlRoutes = require("./html")
+
+// HTML Routes
+router.use("/", htmlRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
